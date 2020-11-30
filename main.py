1da07d447694bdbf98bc65d7f453257a1c5ff5f3
Vladimir Efremov <vladimir.efremov@sravni.ru>
2020-11-30 11:17:01 +0300

    Revert "hometask-3 fix 2"
    This reverts commit 70f479de7293a2506b7982f6ea9a8a976b8f2afe.

--- main.py
+++ main.py
@@ -1,42 +1,54 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from numpy import *
+
+delta = 0.001
 
 def derivation(x, f):
-    delta = 0.0001
-    F = (f(x+delta) - f(x))/delta
-    return round(F, 2)
+    return round((f(x) - f(x - delta)) / delta, 2)
 
-def gradient(function):
-    list_lims = []
-    delta = 0.00001
-    x1 = 10
-    x2 = 1
-    lim_x = (function(x1 + delta, x2) - function(x1, x2)) / delta
-    list_lims.append(round(lim_x, 2))
-    lim_y = (function(x1, x2 + delta) - function(x1, x2)) / delta
-    list_lims.append(round(lim_y, 2))
-    return list_lims
+def gradient(x1, x2, f):
+    return (round(diffx1(x1, x2, f), 2), round(diffx2(x1, x2, f), 2))
 
-def gradient_optimization_one_dim(f):
-    eps = 0.001
-    delta = 0.0001
+def diffx1(x1, x2, f):
+    return (f(x1 + delta, x2) - f(x1, x2)) / delta
+def diffx2(x1, x2, f):
+    return (f(x1, x2 + delta) - f(x1, x2)) / delta
+
+def gradient_optimization_one_dim(x, f):
     for i in range(50):
-        if i == 0:
-            x = 10
-        F = (f(x+delta) - f(x))/delta
-        x -= eps*F
-    return round(x, 2)
+        x = x - delta * derivation(x, f)
+    return x
 
-def gradient_optimization_multi_dim(f):
-    eps = 0.0001
-    delta = 0.000001
+def gradient_optimization_multi_dim(x1, x2, f):
     for i in range(50):
-        if i == 0:
-            x1, x2 = 4, 10
-        F1 = round((f([x1 + delta, x2]) - f([x1, x2])) / delta, 2)
-        F2 = f([x1, x2 + delta]) - f([x1, x2])) / delta
-        x1 -= round(eps * F1, 2)
-        x2 -= eps * F2
-        values = []
-        values.append(round(x1, 2))
-        values.append(round(x2, 2))
-    return values
+        x1 = x1 - delta * diffx1(x1, x2, f)
+        x2 = x2 - delta * diffx1(x1, x2, f)
+    return (x1, x2)
+
+x = 10
+
+def f(x):
+    return (cos(x) + (0.05 * (x ** 3)) + math.log2(x ** 2))
+
+print(derivation(x, f))
+
+def f(x1, x2):
+    return (x1 ** 2) * cos(x2) + 0.05 * x2 ** 3 + 3 * x1 ** 3 * math.log(2,x2 ** 2)
+
+x1 = 1
+x2 = 10
+
+print(gradient(x1, x2, f))
+
+def f(x):
+    return cos(x) + 0.05* x ** 3 + math.log(2, x ** 2)
+
+x = 10
+print(gradient_optimization_one_dim(x, f))
+
+
+def f(x1, x2):
+    return x1 * cos(x2) + 0.05 * x2 ** 3 + 3 * x1 ** 3 * math.log(2, x2 ** 2)
+
+print(gradient_optimization_multi_dim(4, 10, f))
